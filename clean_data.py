3c4096f23e243ef67ac795c9c050a3923c945f39
fhawks97 <114870371+fhawks97@users.noreply.github.com>
2023-08-14 13:49:57 +0100

    Creating script to clean data

---
+++ clean_data.py
@@ -0,0 +1,7 @@
+#########################################################
+# DES: Import raw dataset from '.\scripts\input_files' and export cleaned target dataset into 'cleaned_data' folder.
+#
+#
+# BY: Felix Hawksworth
+#########################################################
+
