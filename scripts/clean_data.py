1c98061d2b1d0006cc3628e7edd07d3ee0237c49
fhawks97 <114870371+fhawks97@users.noreply.github.com>
2023-08-14 21:59:54 +0100

    Testing out AlexNet model with Pytorch

--- scripts/clean_data.py
+++ scripts/clean_data.py
@@ -1,5 +1,6 @@
 #########################################################
-# DES: Import raw dataset from '.\scripts\input_files' and export cleaned target dataset into 'cleaned_data' folder.
+# DES: Import raw dataset from '.\scripts\input_files'
+# Export cleaned dataset into 'cleaned_data' folder.
 #
 #
 # BY: Felix Hawksworth
